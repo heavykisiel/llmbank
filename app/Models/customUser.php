2fb998aafe00510042b93efb7df760b0adf0e459
Lorek Adam <szparax@gmail.com>
2022-12-10 15:25:54 +0100

    dodanie modelu dla table custom_user

--- app/Models/customUser.php
+++ app/Models/customUser.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class customUser extends Model
+class CustomUser extends Model
 {
     use HasFactory;
+
+    protected $table = 'custom_users';
 }
