61cfd976b7615b7dcae87b4df8f24f3edfc9bba7
Lorek Adam <szparax@gmail.com>
2022-12-10 15:23:53 +0100

    Dodanie tabeli custom_users

--- database/migrations/2022_12_08_184729_create_users_table.php
+++ database/migrations/2022_12_08_184729_create_users_table.php
@@ -15,10 +15,22 @@ return new class extends Migration
     {
         Schema::create('custom_users', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
-            #$table->lastlogin();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('token')->default('');;
         });
+
+        DB::table('custom_users')->insert([
+            ['name' => 'user1', 'password' => bcrypt('p@ssword1'), 'email' => 'user1@example.com'],
+            ['name' => 'user2', 'password' => bcrypt('p@ssword2'), 'email' => 'user2@example.com'],
+            ['name' => 'user3', 'password' => bcrypt('p@ssword3'), 'email' => 'user3@example.com'],
+            ['name' => 'user4', 'password' => bcrypt('p@ssword4'), 'email' => 'user4@example.com'],
+            ['name' => 'user5', 'password' => bcrypt('p@ssword5'), 'email' => 'user5@example.com'],
+        ]);
     }
+
+    
 
     /**
      * Reverse the migrations.
