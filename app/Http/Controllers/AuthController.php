7e82cd861e4a96a6452ec091fc4211ef50ed9205
Lorek Adam <szparax@gmail.com>
2022-12-10 15:29:02 +0100

    bufix i dodawanie zależności

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use App\Models\encoding;
 
 class AuthController extends Controller
 {
