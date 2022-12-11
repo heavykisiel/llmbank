cb008dcc75d48b2b7839e72c32fa63f74197fd31
Lorek Adam <szparax@gmail.com>
2022-12-11 21:35:55 +0100

    transfery środków

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+
 use App\Models\customUser;
 use App\Models\BankAccount;
+use App\Models\transaction;
+
 
 class BankController extends Controller
 {
@@ -28,15 +33,60 @@ class BankController extends Controller
 
     public function transfer(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'from' => 'required|exists:bank_account,id',
+            'to' => 'required|exists:bank_account,id',
+            'amount' => 'required|numeric|min:0.01',
+            'description' => 'string|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
         $from = $request->input('from');
         $to = $request->input('to');
         $amount = $request->input('amount');
+        $token = $request->input('token');
+        $description = $request->input('description');
 
-        // tutaj należy dodać kod do przeprowadzenia przelewu
-        // pomiędzy kontami $from i $to o kwocie $amount
+        //sprawdzenie czy konto należy do odpowieniego użytkownika
+        $user = customUser::where('token', $token)->first();
+        
+        $bankAccount = BankAccount::where('user_id', $user->id)
+            ->where('accNUmber',$from)
+            ->first();
+        
+        $toBank = BankAccount::where('accNUmber',$to)
+            ->first();
 
-        return response()->json([
-            'success' => true,
-        ]);
+        if($bankAccount) {
+            if ($toBank == false) { 
+                return response()->json([
+                'errMessage' => 'Konto na które próbujesz zrobić przelew nie istnieje w naszym banku.',],400); 
+            }
+            
+            
+            $transaction = transaction::create([
+                'amount' => $amount,
+                'description' => $description,
+                'from_bank_id' => $from,
+                'to_bank_id' => $to,
+                'currency' => $bankAccount->currency
+            ]);
+
+            $bankAccount->decrement('balance',$amount);
+            $toBank->increment('balance',$amount);
+
+            return response()->json(['succes' => true],200);
+        } else
+        {
+            return response()->json([
+                'errMessage' => 'Próbujesz zrobić przelew z nie swojego konta.',
+            ],400);
+        }        
     }
 }
