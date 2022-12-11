855668151813ce8616ee0e880f37f928c5a0639d
Lorek Adam <szparax@gmail.com>
2022-12-11 20:24:58 +0100

    bugfix: zwaracanie listy a nie tylko jednego konta

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -17,12 +17,26 @@ class BankController extends Controller
         $user = customUser::where('token', $token)->first();
 
         if ($user) {
-            $bankAccounts = BankAccount::where('user_id', $user->id)->first();
+            $bankAccounts = BankAccount::where('user_id', $user->id)->get();
 
             return response()->json($bankAccounts,200);
         } else
         {
             return response()->json(['errorMessage'=>'nieprawidłowy token'],400);
         }
+    }
+
+    public function transfer(Request $request)
+    {
+        $from = $request->input('from');
+        $to = $request->input('to');
+        $amount = $request->input('amount');
+
+        // tutaj należy dodać kod do przeprowadzenia przelewu
+        // pomiędzy kontami $from i $to o kwocie $amount
+
+        return response()->json([
+            'success' => true,
+        ]);
     }
 }
